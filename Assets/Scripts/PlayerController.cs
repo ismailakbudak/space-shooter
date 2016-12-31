51e1ef2bbc24e69620b93e16e84378e73230aeba
İsmail Akbudak <ismail.akbudak@lab2023.com>
2016-12-31 14:25:22 +0200

    Add player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed, tilt;
+	public Boundary boundary;
+
+	private Rigidbody rigidbody;
+
+	void Start (){
+		rigidbody = GetComponent<Rigidbody>(); 
+	}
+
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.velocity = movement * speed;
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+}
