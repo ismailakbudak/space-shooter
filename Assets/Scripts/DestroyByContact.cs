7eef3d71f97c35166e297515753bdf4b6c111b58
İsmail Akbudak <ismail.akbudak@lab2023.com>
2017-01-02 22:37:19 +0300

    Add asteroid object move script

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion, playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
+
 		if (other.name == "Boundary")
 			return;
+
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.name == "Player")
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		
 		Destroy(other.gameObject);
 		Destroy(gameObject);
