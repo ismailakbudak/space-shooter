9a9ebb6012b3e48e06fe64bfe211ea75bddc8f34
İsmail Akbudak <ismail.akbudak@lab2023.com>
2017-01-02 22:25:34 +0300

    Add asteroid objects and destroy by concat of astreod script

---
+++ Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>(); 
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;	
+	}
+}
